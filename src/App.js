8d9f8ba27558f2fbf87b613f62a1d96a4e5df526
stealthman22 <tdotonline@gmail.com>
2020-09-08 02:09:23 +0000

    adding history function, encountered some errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from "react";
 // Controlled components
 // Changed to functional component
 function Square(props) {
-	 {
 		// passing props
 		return (
 			// remember => {} when working with event handlers.
@@ -16,36 +15,15 @@ function Square(props) {
 	
 }
 
-class Board extends React.Component {
-	// A function of Constructors is used to initialize state
-	// super must always be called when defining constructor of a subclass
-constructor(props) {
-	super (props);
-	this.state = {
-		squares: Array(9).fill(null),
-	};
-}
-
-// fucntion that onclick in Square calls
-//  Slice preserves immutability, same as assign for objects
-//  assign works just like spread on objects
-// simplitiy change detection determine rerender (pure components)
-handleClick(i) {
-	const squares = this.state.squares.slice();
-	squares[i] = 'X';
-	this.setState({squares: squares})
-}
-
+class Board extends React.Component {	
 	renderSquare(i) {
 		//  making value to be updated by state!
-		return <Square value={this.state.squares[i]} 
-			onClick={() => this.handleClick(i)}
+		return <Square value={this.state.props[i]} 
+			onClick={() => this.props.onClick(i)}
 		/>;
 	}
 
 	render() {
-		const status = "Next player: X";
-
 		return (
 			<div>
 				<div className="status">{status}</div>
@@ -70,11 +48,77 @@ handleClick(i) {
 }
 
 class Game extends React.Component {
+	// A function of Constructors is used to initialize state
+	// super must always be called when defining constructor of a subclass
+
+	constructor(props) {
+		super (props)
+		this.state = {
+			history: [{
+				squares: Array(9).fill(null)
+			}],
+			xIsNext: true
+		}
+	}
+
+// fucntion that onclick in Square calls
+//  Slice preserves immutability, same as assign for objects
+//  assign works just like spread on objects
+// simplitiy change detection determine rerender (pure components)
+handleClick(i) {
+	const history = this.state.history
+	const current = history[history.length - 1]
+	const squares = current.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
+	squares[i] = this.state.xIsNext ? 'X' : 'O';
+	this.setState({squares: squares,
+		history: history.concat([{
+			squares: squares
+		}]),
+		xIsNext: !this.state.xIsNext
+		})
+}
+
+
 	render() {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const winner = calculateWinner(current.squares);
+
+		// showing past moves as buttons
+		const moves = history.map((step, move) => {
+			const desc = move ?
+			`Go to move #${move}`:
+			'Go to game start'
+		})
+		return (
+			<li>
+				<button onClick={() => this.jumpTo(move)}>
+				{desc}
+				</button>
+			</li>
+		)
+		let status;
+		if (winner) {
+			status = `Winner ${winner}`
+		} else {
+			status=`Next Player ${this.state.xIsNext ? 'X': 'O'}`
+		}
+
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board />
+					<Board 
+						squares={current.squares}
+						onClick={(i) => this.handleClick(i)}
+					/>
+					{/* add new element for moves */}
+					<div className='game-info'>
+						<div>{status}</div>
+						<ol>{moves}</ol>
+					</div>
 				</div>
 			</div>
 		);
@@ -83,5 +127,25 @@ class Game extends React.Component {
 
 //  ===================
 
+// helper function for tic-tac-toe winner
+function calculateWinner(squares) {
+	const lines = [
+	  [0, 1, 2],
+	  [3, 4, 5],
+	  [6, 7, 8],
+	  [0, 3, 6],
+	  [1, 4, 7],
+	  [2, 5, 8],
+	  [0, 4, 8],
+	  [2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+	  const [a, b, c] = lines[i];
+	  if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+		return squares[a];
+	  }
+	}
+	return null;
+}
 
 export default Game;

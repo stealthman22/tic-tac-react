83e34b86329039bdc6164f7f1e99583c736dccaa
stealthman22 <tdotonline@gmail.com>
2020-09-09 04:16:16 +0000

    few changes

--- src/App.js
+++ src/App.js
@@ -94,8 +94,8 @@ class Game extends React.Component {
 	jumpTo(step) {
 		this.setState({
 			stepNumber: step,
-			xIsNext: (step % 2) === 0,
-		})
+			xIsNext: step % 2 === 0
+		});
 	}
 
 	render() {
@@ -148,53 +148,17 @@ class Game extends React.Component {
 // helper function for tic-tac-toe winner
 function calculateWinner(squares) {
 	const lines = [
-		[
-			0,
-			1,
-			2
-		],
-		[
-			3,
-			4,
-			5
-		],
-		[
-			6,
-			7,
-			8
-		],
-		[
-			0,
-			3,
-			6
-		],
-		[
-			1,
-			4,
-			7
-		],
-		[
-			2,
-			5,
-			8
-		],
-		[
-			0,
-			4,
-			8
-		],
-		[
-			2,
-			4,
-			6
-		]
+		[ 0, 1, 2 ],
+		[ 3, 4, 5 ],
+		[ 6, 7, 8 ],
+		[ 0, 3, 6 ],
+		[ 1, 4, 7 ],
+		[ 2, 5, 8 ],
+		[ 0, 4, 8 ],
+		[ 2, 4, 6 ]
 	];
 	for (let i = 0; i < lines.length; i++) {
-		const [
-			a,
-			b,
-			c
-		] = lines[i];
+		const [ a, b, c ] = lines[i];
 		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
 			return squares[a];
 		}
